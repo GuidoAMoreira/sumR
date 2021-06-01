d0a37bb6b046ff7142365932d4f0aa58b6247e35
Guido Alberti Moreira <42647037+GuidoAMoreira@users.noreply.github.com>
2021-06-01 14:39:07 +0100

    Getting it CRAN ready

--- src/init.c
+++ src/init.c
@@ -4,6 +4,9 @@
 
 void R_init_sumR(DllInfo *info)
 {
+  R_registerRoutines(info, NULL, NULL, NULL, NULL);
+  R_useDynamicSymbols(info, TRUE);
+
   /* used by external packages linking to internal sumR code from C */
   R_RegisterCCallable("sumR", "infiniteSum_", (DL_FUNC) &infiniteSum_);
   R_RegisterCCallable("sumR", "infiniteSumToThreshold_",
